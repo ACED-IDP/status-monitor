a52a1666389e8ee044787c2957d3cb8047d886e5
Liam Beckman <lbeckman314@gmail.com>
2024-02-05 12:13:32 -0800

    Create hide-sections.js

---
+++ hide-sections.js
@@ -0,0 +1,18 @@
+// Script to hide sections from the status page
+// Credit: bvenkysubbu
+// Source: https://github.com/upptime/upptime/discussions/447
+
+function contains(t, n) {
+    var e = document.querySelectorAll(t);
+    return Array.prototype.filter.call(e, function(t) {
+        return RegExp(n).test(t.textContent)
+    })
+}
+window.onload = function() {
+    setTimeout(function() {
+        foundDivsScheduledMaintenance = contains('section', 'Past Scheduled Maintenance');
+        foundDivsScheduledMaintenance[0].remove(); // Removes Active Incidents Section
+        foundDivsPastIncidents = contains('section', 'Past Incidents');
+        foundDivsPastIncidents[0].remove(); // Removes Past Incidents Section
+    }, 5000);
+}
